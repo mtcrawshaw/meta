19300c905b9137b3d8be6bea637a2ef85be3c78c
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-01-21 07:50:50 -0500

    Added simple training call in main.py, causes error with observations outside of obs space.

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+import traceback
+
+import ray
+from ray import tune
+from ray.tune.registry import register_env
 from metaworld.benchmarks import ML1
 
 
-print(ML1.available_tasks())  # Check out the available tasks
+def main():
+    """ Main function for main.py. """
 
-env = ML1.get_train_tasks('pick-place-v1')  # Create an environment with task `pick_place`
-tasks = env.sample_tasks(1)  # Sample a task (in this case, a goal variation)
-env.set_task(tasks[0])  # Set task
+    # Choose environment name.
+    env_name = ML1.available_tasks()[0]
+    print("Env name: %s" % env_name)
 
-obs = env.reset()  # Reset environment
-a = env.action_space.sample()  # Sample an action
-obs, reward, done, info = env.step(a)  # Step the environoment with the sampled random action
+    # Define environment creator.
+    def env_creator(env_config=None):
+        env = ML1.get_train_tasks(env_name) 
+        tasks = env.sample_tasks(1) 
+        env.set_task(tasks[0]) 
+        obs_space = env.observation_space
+        high = obs_space.high
+        low = obs_space.low
+        #print("\n\n\nObservation space, high, low: %s, %s, %s\n\n\n" % (str(obs_space), str(high), str(low)))
+        return env
+
+    # Register environment and run training.
+    ray.init()
+    register_env(env_name, env_creator) 
+    tune.run(
+        "PPO",
+        stop={"timesteps_total": 10000},
+        config={
+            "env": env_name,
+            "num_gpus": 0,
+            "num_workers": 1,
+            "lr": 0.001,
+            "eager": False,
+        },
+    )
+
+if __name__ == "__main__":
+    main()
