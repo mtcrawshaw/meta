9a01f95a75f28ab78f492bce7c6e5ac63ab3b45c
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-01-22 10:56:55 -0500

    Added a bunch of debug code for metaworld environments. Now prints when/where observations lie outside of obs space.

--- main.py
+++ main.py
@@ -1,43 +1,74 @@
-import traceback
-
+import numpy as np
 import ray
 from ray import tune
+from ray.rllib.agents.ppo import PPOTrainer
 from ray.tune.registry import register_env
 from metaworld.benchmarks import ML1
+
+ENV_NAME = ML1.available_tasks()[0]
+
+DEBUG = True
+
+def env_creator(env_config=None):
+    """ Environment creator function to register environment. """
+
+    env = ML1.get_train_tasks(ENV_NAME)
+    tasks = env.sample_tasks(1)
+    env.set_task(tasks[0])
+    return env
+
+def train_fn(config, reporter):
+    """ Train function to pass to tune.run()."""
+
+    agent = PPOTrainer(env=ENV_NAME, config=config)
+    for _ in range(10000):
+        result = agent.train()
+        reporter(**result)
 
 
 def main():
     """ Main function for main.py. """
-
-    # Choose environment name.
-    env_name = ML1.available_tasks()[0]
-    print("Env name: %s" % env_name)
-
-    # Define environment creator.
-    def env_creator(env_config=None):
-        env = ML1.get_train_tasks(env_name) 
-        tasks = env.sample_tasks(1) 
-        env.set_task(tasks[0]) 
-        obs_space = env.observation_space
-        high = obs_space.high
-        low = obs_space.low
-        #print("\n\n\nObservation space, high, low: %s, %s, %s\n\n\n" % (str(obs_space), str(high), str(low)))
-        return env
+    global ENV_NAME
 
     # Register environment and run training.
-    ray.init()
-    register_env(env_name, env_creator) 
-    tune.run(
-        "PPO",
-        stop={"timesteps_total": 10000},
-        config={
-            "env": env_name,
-            "num_gpus": 0,
-            "num_workers": 1,
+    if DEBUG:
+
+        env_names = ML1.available_tasks()
+
+        for i in range(len(env_names)):
+            ENV_NAME = env_names[i]
+            env = env_creator()
+            low = env.observation_space.low
+            high = env.observation_space.high
+            obs = env.reset()
+            broken_indices = []
+
+            for _ in range(1000):
+                if np.any(np.logical_or(low > obs, obs > high)):
+                    current_indices = np.argwhere(np.logical_or(low > obs, obs > high))
+                    current_indices = current_indices.reshape((-1,)).tolist()
+                    for current_index in current_indices:
+                        if current_index not in broken_indices:
+                            broken_indices.append(current_index)
+
+                a = env.action_space.sample()
+                obs, reward, done, info = env.step(a)
+
+            broken_indices = sorted(broken_indices)
+            print("%s broken indices: %r" % (ENV_NAME, broken_indices))
+
+    else:
+        ray.init()
+        register_env(ENV_NAME, env_creator)
+        config = {
             "lr": 0.001,
-            "eager": False,
-        },
-    )
+            "num_workers": 0,
+        }
+        tune.run(
+            train_fn,
+            stop={"timesteps_total": 10000},
+            config=config,
+        )
 
 if __name__ == "__main__":
     main()
