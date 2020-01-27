f530085acd40997daf13c63f0fe0ab83c75b1097
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-01-27 11:36:34 -0500

    Got rid of RLlib due to observation space incompatibility with MetaWorld.

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
 import argparse
-import random
 
-import numpy as np
-from gym.spaces.box import Box
-import ray
-from ray import tune
-from ray.rllib.agents.ppo import PPOTrainer
-from ray.tune.registry import register_env
 from metaworld.benchmarks import ML1
 
 from utils import validate_args
-
-
-def env_creator(env_config):
-    """ Environment creator function to register environment. """
-
-    # Get environment and set task.
-    env = ML1.get_train_tasks(env_config["env_name"])
-    tasks = env.sample_tasks(1)
-    env.set_task(tasks[0])
-
-    # HARDCODE: Expand observation space.
-    obs_shape = env.observation_space.shape
-    env.active_env.observation_space = Box(
-        low=-float("inf"), high=float("inf"), shape=obs_shape
-    )
-
-    return env
-
-
-def train_fn(config, reporter):
-    """ Train function to pass to tune.run()."""
-
-    agent = PPOTrainer(env=config["env_config"]["env_name"], config=config)
-    for _ in range(config["env_config"]["timesteps_total"]):
-        result = agent.train()
-
-        # Suppress output, for now.
-        # reporter(**result)
 
 
 def main(args):
@@ -45,25 +10,20 @@ def main(args):
 
     if args.algo == "PPO":
 
-        # Register environment and run training.
-        ray.init()
-        env_name = random.choice(ML1.available_tasks())
-        register_env(env_name, env_creator)
-        config = {
-            "env_config": {
-                "env_name": env_name,
-                "timesteps_total": 3,
-            },
-            "lr": 0.001,
-            "num_workers": 0,
-        }
-        tune.run(
-            train_fn, config=config,
-        )
+        # Get environment and set task.
+        env_name = args.benchmark
+        env = ML1.get_train_tasks(env_config["env_name"])
+        tasks = env.sample_tasks(1)
+        env.set_task(tasks[0])
+
+        # Run environment.
+        for _ in range(args.timesteps):
+            a = env.action_space.sample()
+            obs, reward, done, info = env.step(a)
 
     elif args.algo == "MAML":
 
-        pass
+        raise NotImplementedError
 
     else:
         raise ValueError("Unknown algorithm: '%s'." % args.algo)
