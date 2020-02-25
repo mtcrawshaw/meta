0dc98d63c3d848bdf0fb360bae8c7df06ea6277a
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-02-25 10:44:36 -0500

    Moved rollout collection into separate function for testing.

--- meta/train.py
+++ meta/train.py
@@ -1,11 +1,87 @@
 import argparse
+from typing import Any, List
 
+import numpy as np
 import torch
 import gym
+from gym import Env
 
 from meta.ppo import PPOPolicy
 from meta.storage import RolloutStorage
 from meta.utils import get_metaworld_env_names, print_metrics
+
+
+def collect_rollout(
+    env: Env, policy: PPOPolicy, rollout_length: int, initial_obs: Any
+) -> List[RolloutStorage]:
+    """
+    Run environment and collect rollout information (observations, rewards, actions,
+    etc.) into a RolloutStorage object, one for each episode.
+
+    Parameters
+    ----------
+    env : Env
+        Environment to run.
+    policy : PPOPolicy
+        Policy to sample actions with.
+    rollout_length : int
+        Combined length of episodes in rollout (i.e. number of steps for a single
+        update).
+    initial_obs : Any
+        Initial observation returned from call to env.reset().
+
+    Returns
+    -------
+    rollouts : List[RolloutStorage]
+        List of RolloutStorage objects, one for each episode.
+    obs : Any
+        Last observation from rollout, to be used as the initial observation for the
+        next rollout.
+    """
+
+    rollouts = []
+    rollouts.append(
+        RolloutStorage(
+            rollout_length=rollout_length,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+        )
+    )
+    rollouts[-1].set_initial_obs(initial_obs)
+
+    # Rollout loop.
+    rollout_step = 0
+    for total_rollout_step in range(rollout_length):
+
+        # Sample actions.
+        with torch.no_grad():
+            value_pred, action, action_log_prob = policy.act(
+                rollouts[-1].obs[rollout_step]
+            )
+
+        # Perform step and record in ``rollouts``.
+        # We cast the action to a numpy array here because policy.act() returns
+        # it as a torch.Tensor. Less conversion back and forth this way.
+        obs, reward, done, info = env.step(action.numpy())
+        rollouts[-1].add_step(obs, action, action_log_prob, value_pred, reward)
+
+        # Reinitialize environment and set first observation, if finished.
+        if done:
+            obs = env.reset()
+            if total_rollout_step < rollout_length - 1:
+                rollouts.append(
+                    RolloutStorage(
+                        rollout_length=rollout_length,
+                        observation_space=env.observation_space,
+                        action_space=env.action_space,
+                    )
+                )
+                rollouts[-1].set_initial_obs(obs)
+                rollout_step = 0
+
+        rollout_step += 1
+
+    return rollouts, obs
 
 
 def train(args: argparse.Namespace):
@@ -48,18 +124,9 @@ def train(args: argparse.Namespace):
         hidden_size=args.hidden_size,
         normalize_advantages=args.normalize_advantages,
     )
-    rollouts = []
-    rollouts.append(
-        RolloutStorage(
-            rollout_length=args.rollout_length,
-            observation_space=env.observation_space,
-            action_space=env.action_space,
-        )
-    )
 
     # Initialize environment and set first observation.
-    obs = env.reset()
-    rollouts[-1].set_initial_obs(obs)
+    initial_obs = env.reset()
 
     # Training loop.
     metric_keys = ["action", "value", "entropy", "total", "reward"]
@@ -73,48 +140,19 @@ def train(args: argparse.Namespace):
 
     for iteration in range(args.num_iterations):
 
-        # Rollout loop.
-        rollout_reward = 0.0
-        rollout_step = 0
-        for total_rollout_step in range(args.rollout_length):
-
-            # Sample actions.
-            with torch.no_grad():
-                value_pred, action, action_log_prob = policy.act(
-                    rollouts[-1].obs[rollout_step]
-                )
-
-            # Perform step and record in ``rollouts``.
-            # We cast the action to a numpy array here because policy.act() returns
-            # it as a torch.Tensor. Less conversion back and forth this way.
-            obs, reward, done, info = env.step(action.numpy())
-            rollouts[-1].add_step(obs, action, action_log_prob, value_pred, reward)
-            rollout_reward += reward
-
-            # Reinitialize environment and set first observation, if finished.
-            if done:
-                rollouts.append(
-                    RolloutStorage(
-                        rollout_length=args.rollout_length,
-                        observation_space=env.observation_space,
-                        action_space=env.action_space,
-                    )
-                )
-                obs = env.reset()
-                rollouts[-1].set_initial_obs(obs)
-                rollout_step = 0
-
-                metrics["reward"] = update_metric(
-                    metrics["reward"], rollout_reward, args.ema_alpha
-                )
-                rollout_reward = 0.0
-
-            rollout_step += 1
-
-        # Compute update.
+        # Sample rollouts and compute update.
+        rollouts, last_obs = collect_rollout(
+            env, policy, args.rollout_length, initial_obs
+        )
+        initial_obs = last_obs
         loss_items = policy.update(rollouts)
 
         # Update and print metrics.
+        episode_rewards = [float(torch.sum(rollout.rewards)) for rollout in rollouts]
+        avg_episode_reward = np.mean(episode_rewards)
+        metrics["reward"] = update_metric(
+            metrics["reward"], avg_episode_reward, args.ema_alpha
+        )
         for loss_key, loss_item in loss_items.items():
             metrics[loss_key] = update_metric(
                 metrics[loss_key], loss_item, args.ema_alpha
@@ -125,133 +163,3 @@ def train(args: argparse.Namespace):
         # This is to ensure that printed out values don't get overwritten.
         if iteration == args.num_iterations - 1:
             print("")
-
-        # Clear rollout storage. If we're in the middle of an episode, call
-        # RolloutStorage.clear() to retain observation.
-        if done:
-            rollouts = []
-            rollouts.append(
-                RolloutStorage(
-                    rollout_length=args.rollout_length,
-                    observation_space=env.observation_space,
-                    action_space=env.action_space,
-                )
-            )
-        else:
-            rollouts = [rollouts[-1]]
-            rollouts[-1].clear()
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--num_iterations",
-        type=int,
-        default=10000,
-        help="Number of PPO training iterations (outer loop).",
-    )
-    parser.add_argument(
-        "--rollout_length",
-        type=int,
-        default=1024,
-        help="Length of rollout (inner loop).",
-    )
-    parser.add_argument(
-        "--env_name",
-        type=str,
-        default="CartPole-v1",
-        help="Which environment to run. Can be a Gym or a Meta-World environment",
-    )
-    parser.add_argument(
-        "--num_ppo_epochs",
-        type=int,
-        default=4,
-        help="Number of training steps to perform on each rollout.",
-    )
-    parser.add_argument(
-        "--lr", type=float, default=3e-4, help="Learning rate for training.",
-    )
-    parser.add_argument(
-        "--eps",
-        type=float,
-        default=1e-5,
-        help="Epsilon value for numerical stability. Usually 1e-8",
-    )
-    parser.add_argument(
-        "--value_loss_coeff",
-        type=float,
-        default=0.5,
-        help="Coefficient on value loss in training objective.",
-    )
-    parser.add_argument(
-        "--entropy_loss_coeff",
-        type=float,
-        default=0.01,
-        help="Coefficient on entropy loss in training objective.",
-    )
-    parser.add_argument(
-        "--gamma", type=float, default=0.99, help="Discount factor.",
-    )
-    parser.add_argument(
-        "--gae_lambda",
-        type=float,
-        default=0.95,
-        help="Lambda parameter for GAE (used in equation (11) of PPO paper).",
-    )
-    parser.add_argument(
-        "--minibatch_size", type=int, default=256, help="Size of each SGD minibatch.",
-    )
-    parser.add_argument(
-        "--clip_param",
-        type=float,
-        default=0.2,
-        help="Clipping parameter for PPO surrogate loss.",
-    )
-    parser.add_argument(
-        "--max_grad_norm",
-        type=float,
-        default=0.5,
-        help="Maximum norm of loss gradients for update.",
-    )
-    parser.add_argument(
-        "--clip_value_loss",
-        default=False,
-        action="store_true",
-        help="Whether or not to clip the value loss.",
-    )
-    parser.add_argument(
-        "--num_layers",
-        type=int,
-        default=3,
-        help="Number of layres in actor/critic model.",
-    )
-    parser.add_argument(
-        "--hidden_size",
-        type=int,
-        default=64,
-        help="Hidden size of actor/critic network.",
-    )
-    parser.add_argument(
-        "--no_advantage_normalization",
-        dest="normalize_advantages",
-        default=True,
-        action="store_false",
-        help="Do not normalize advantages.",
-    )
-    parser.add_argument(
-        "--ema_alpha",
-        type=float,
-        default=0.95,
-        help="Alpha value for exponential moving averages.",
-    )
-    parser.add_argument(
-        "--print_freq",
-        type=int,
-        default=10,
-        help="Number of training iterations between metric printing.",
-    )
-
-    args = parser.parse_args()
-
-    main(args)
