4aff51dc9b509f3c6466981c3e510f2a566dd01a
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-03-09 10:49:06 -0400

    Added test for train.collect_rollouts().

---
+++ meta/tests/test_train.py
@@ -0,0 +1,49 @@
+from math import log
+
+import torch
+
+from meta.train import collect_rollout
+from meta.utils import get_env
+from meta.tests.utils import get_policy, DEFAULT_SETTINGS
+from meta.tests.envs import UniquePolicy
+
+
+def test_collect_rollout_values():
+    """
+    Test the values of the returned RolloutStorage object from train.collect_rollout().
+    """
+
+    settings = dict(DEFAULT_SETTINGS)
+    settings["env_name"] = "unique-env"
+
+    env = get_env(settings["env_name"])
+    policy = UniquePolicy()
+    initial_obs = env.reset()
+    rollouts, _ = collect_rollout(env, policy, settings["rollout_length"], initial_obs)
+
+    # Check if rollout info came from UniqueEnv.
+    TOL = 1e-6
+    for step in range(rollouts.rollout_step):
+
+        obs = rollouts.obs[step]
+        value_pred = rollouts.value_preds[step]
+        action = rollouts.actions[step]
+        action_log_prob = rollouts.action_log_probs[step]
+        reward = rollouts.rewards[step]
+
+        # Check shapes.
+        assert obs.shape == torch.Size([1])
+        assert value_pred.shape == torch.Size([])
+        assert action.shape == torch.Size([1])
+        assert action_log_prob.shape == torch.Size([])
+        assert reward.shape == torch.Size([])
+
+        # Check consistency of values.
+        assert float(obs) == float(value_pred)
+        assert float(action) - int(action) == 0 and int(action) in env.action_space
+        assert (
+            float(action_log_prob)
+            - log(policy.policy_network.action_probs(float(obs))[int(action)])
+            < TOL
+        )
+        assert float(obs) == float(reward)
