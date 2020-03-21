13ece604b7b27324b7443faae74364ea15cf8139
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-03-21 11:19:55 -0400

    Removing --num-processes from run.sh.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 main.py --num-processes 1 --minibatch-size 5 --env-name CartPole-v1
+python3 main.py --minibatch-size 5 --env-name CartPole-v1 --compare-output-metrics non_repeating_metrics.pkl
