78e457dc4d4da6cb1994077eca9bad1421947457
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-06-17 21:12:19 -0400

    Added plotting of multiple metrics.

--- meta/plot.py
+++ meta/plot.py
@@ -11,27 +11,32 @@ def plot(metrics_state: Dict[str, Dict[str, List[float]]], plot_path: str) -> No
     Plot the metrics given in ``metrics_history``, store image at ``plot_path``.
     """
 
+    # Create a subplot for each metric.
+    fig, axs = plt.subplots(len(metrics_state), figsize=(12.8, 9.6))
+    fig.suptitle("Training Metrics")
+
     # Plot each metric on a separate curve.
-    legend = []
-    for metric_name, metric_state in metrics_state.items():
+    for i, (metric_name, metric_state) in enumerate(metrics_state.items()):
 
         mean_array = np.array(metric_state["mean"])
         stdev_array = np.array(metric_state["stdev"])
         assert len(mean_array) == len(stdev_array)
+        legend = []
 
         # Plot mean.
-        plt.plot(np.arange(len(mean_array)), mean_array)
+        axs[i].plot(np.arange(len(mean_array)), mean_array)
         legend.append("%s mean" % metric_name)
 
         # Plot mean + stdev and mean - stdev.
         upper_dev_array = mean_array + stdev_array
         lower_dev_array = mean_array - stdev_array
-        plt.plot(np.arange(len(mean_array)), upper_dev_array)
-        plt.plot(np.arange(len(mean_array)), lower_dev_array)
+        axs[i].plot(np.arange(len(mean_array)), upper_dev_array)
+        axs[i].plot(np.arange(len(mean_array)), lower_dev_array)
         legend.append("%s mean + stdev" % metric_name)
         legend.append("%s mean - stdev" % metric_name)
 
-    plt.legend(legend, loc="upper right")
+        # Add legend to subplot.
+        axs[i].legend(legend, loc="upper right")
 
     # Save out plot.
     plt.savefig(plot_path)
