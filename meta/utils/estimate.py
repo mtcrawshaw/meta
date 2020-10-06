33ed5f64dd2bb478c5baa7468238415e86c50e58
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-10-06 10:15:19 -0400

    Updated code/tests for keeping running stats for Tensors.

--- meta/utils/estimate.py
+++ meta/utils/estimate.py
@@ -12,52 +12,84 @@ import torch
 # used for EMA.
 EMA_THRESHOLD = 100
 EMA_ALPHA = 0.99
+
+
 def single_update(m: torch.Tensor, v: torch.Tensor, n: int) -> torch.Tensor:
     if n <= EMA_THRESHOLD:
         return (m * (n - 1) + v) / n
     else:
-        return m * EMA_ALPHA + v * (1. - EMA_ALPHA)
+        return m * EMA_ALPHA + v * (1.0 - EMA_ALPHA)
 
 
 class RunningMean:
     """ Utility class to compute running mean of torch.Tensor. """
 
-    def __init__(self, shape: Tuple[int, ...]) -> None:
-        """ Init function for RunningMean. """
+    def __init__(self, shape: Tuple[int, ...] = None, condense: bool = False) -> None:
+        """
+        Init function for RunningMean. `shape` is shape of input tensors, which is only
+        needed when `condense=False`. If `condense=True`, then we treat all elements of
+        the input tensors as samples of the same variable, and compute a single-valued
+        mean.
+        """
 
         self.num_steps = 0
-        self.shape = shape
-        self.mean = torch.zeros(*shape)
+        self.condense = condense
+        if self.condense:
+            self.mean = torch.zeros(())
+        else:
+            assert shape is not None
+            self.shape = shape
+            self.mean = torch.zeros(*shape)
 
     def update(self, val: torch.Tensor) -> None:
         """ Update running mean with new value. """
 
         self.num_steps += 1
-        self.mean = single_update(self.mean, val, self.num_steps)
+        if self.condense:
+            self.mean = single_update(self.mean, torch.mean(val), self.num_steps)
+        else:
+            self.mean = single_update(self.mean, val, self.num_steps)
 
 
 class RunningMeanStdev:
     """
     Utility class to compute running mean and standard deviation of torch.Tensor. We do
     this by keeping a running estimate of the mean and a running estimate of the mean of
-    the square, and using the formula `Var[X] = E[X^2] - E[X]^2.
+    the square, and using the formula `Var[X] = E[X^2] - E[X]^2. `shape` is shape of
+    input tensors, which is only needed when `condense=False`. If `condense=True`, then
+    we treat all elements of the input tensors as samples of the same variable, and
+    compute a single-valued mean and standard deviation.
     """
 
-    def __init__(self, shape: Tuple[int, ...]) -> None:
+    def __init__(self, shape: Tuple[int, ...] = None, condense: bool = False) -> None:
         """ Init function for RunningMeanStd. """
 
         self.num_steps = 0
-        self.shape = shape
-        self.mean = torch.zeros(*shape)
-        self.square_mean = torch.zeros(*shape)
-        self.var = torch.zeros(*shape)
-        self.stdev = torch.zeros(*shape)
+        self.condense = condense
+        if self.condense:
+            self.mean = torch.zeros(())
+            self.square_mean = torch.zeros(())
+            self.var = torch.zeros(())
+            self.stdev = torch.zeros(())
+        else:
+            assert shape is not None
+            self.shape = shape
+            self.mean = torch.zeros(*shape)
+            self.square_mean = torch.zeros(*shape)
+            self.var = torch.zeros(*shape)
+            self.stdev = torch.zeros(*shape)
 
     def update(self, val: torch.Tensor) -> None:
         """ Update running mean with new value. """
 
         self.num_steps += 1
-        self.mean = single_update(self.mean, val, self.num_steps)
-        self.square_mean = single_update(self.square_mean, val ** 2, self.num_steps)
+        if self.condense:
+            self.mean = single_update(self.mean, torch.mean(val), self.num_steps)
+            self.square_mean = single_update(
+                self.square_mean, torch.mean(val ** 2), self.num_steps
+            )
+        else:
+            self.mean = single_update(self.mean, val, self.num_steps)
+            self.square_mean = single_update(self.square_mean, val ** 2, self.num_steps)
         self.var = self.square_mean - self.mean ** 2
         self.stdev = torch.sqrt(self.var)
--- tests/utils/test_estimate.py
+++ tests/utils/test_estimate.py
@@ -25,7 +25,7 @@ def test_mean_arithmetic():
     mean = RunningMean(shape=shape)
     for i in range(len(data)):
         mean.update(data[i])
-        assert torch.allclose(mean.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1], dim=0))
 
 
 def test_mean_rand_arithmetic():
@@ -39,11 +39,11 @@ def test_mean_rand_arithmetic():
     mean = RunningMean(shape=shape)
     for i in range(len(data)):
         mean.update(data[i])
-        assert torch.allclose(mean.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1], dim=0))
 
 
 def test_mean_ema():
-    """ Test computation of EMA in Running Mean. """
+    """ Test computation of EMA in RunningMean. """
 
     # Set up case.
     shape = (3, 3)
@@ -59,32 +59,32 @@ def test_mean_ema():
     mean = RunningMean(shape=shape)
     for i in range(EMA_THRESHOLD):
         mean.update(data[i])
-        assert torch.allclose(mean.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1], dim=0))
 
     expected_mean = torch.ones(*shape)
     for i in range(EMA_THRESHOLD, len(data)):
         mean.update(data[i])
-        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1. - EMA_ALPHA)
+        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1.0 - EMA_ALPHA)
         assert torch.allclose(mean.mean, expected_mean)
 
 
 def test_mean_rand_ema():
-    """ Test computation of EMA in Running Mean, with random values. """
+    """ Test computation of EMA in RunningMean, with random values. """
 
     # Set up case.
     shape = (10, 10)
-    data = torch.rand(EMA_THRESHOLD + 200)
+    data = torch.rand(EMA_THRESHOLD + 200, *shape)
 
     # Perform and check computation.
     mean = RunningMean(shape=shape)
     for i in range(EMA_THRESHOLD):
         mean.update(data[i])
-        assert torch.allclose(mean.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1], dim=0))
 
     expected_mean = mean.mean
     for i in range(EMA_THRESHOLD, len(data)):
         mean.update(data[i])
-        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1. - EMA_ALPHA)
+        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1.0 - EMA_ALPHA)
         assert torch.allclose(mean.mean, expected_mean)
 
 
@@ -103,9 +103,11 @@ def test_mean_std_arithmetic():
     stats = RunningMeanStdev(shape=shape)
     for i in range(len(data)):
         stats.update(data[i])
-        assert torch.allclose(stats.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1], dim=0))
         if i > 0:
-            assert torch.allclose(stats.stdev, torch.std(data[0:i+1], dim=0, unbiased=False))
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], dim=0, unbiased=False)
+            )
 
 
 def test_mean_std_rand_arithmetic():
@@ -122,9 +124,11 @@ def test_mean_std_rand_arithmetic():
     stats = RunningMeanStdev(shape=shape)
     for i in range(len(data)):
         stats.update(data[i])
-        assert torch.allclose(stats.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1], dim=0))
         if i > 0:
-            assert torch.allclose(stats.stdev, torch.std(data[0:i+1], dim=0, unbiased=False), atol=TOL)
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], dim=0, unbiased=False), atol=TOL
+            )
 
 
 def test_mean_std_ema():
@@ -144,16 +148,20 @@ def test_mean_std_ema():
     stats = RunningMeanStdev(shape=shape)
     for i in range(EMA_THRESHOLD):
         stats.update(data[i])
-        assert torch.allclose(stats.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1], dim=0))
         if i > 0:
-            assert torch.allclose(stats.stdev, torch.std(data[0:i+1], dim=0, unbiased=False), atol=TOL)
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], dim=0, unbiased=False), atol=TOL
+            )
 
     expected_mean = torch.ones(*shape)
     expected_square_mean = torch.ones(*shape)
     for i in range(EMA_THRESHOLD, len(data)):
         stats.update(data[i])
-        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1. - EMA_ALPHA)
-        expected_square_mean = expected_square_mean * EMA_ALPHA + data[i] ** 2 * (1. - EMA_ALPHA)
+        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1.0 - EMA_ALPHA)
+        expected_square_mean = expected_square_mean * EMA_ALPHA + data[i] ** 2 * (
+            1.0 - EMA_ALPHA
+        )
         expected_stdev = torch.sqrt(expected_square_mean - expected_mean ** 2)
         assert torch.allclose(stats.mean, expected_mean)
         assert torch.allclose(stats.stdev, expected_stdev)
@@ -167,23 +175,235 @@ def test_mean_std_rand_ema():
 
     # Set up case.
     shape = (10, 10)
-    data = torch.rand(EMA_THRESHOLD + 200)
+    data = torch.rand(EMA_THRESHOLD + 200, *shape)
 
     # Perform and check computation.
     stats = RunningMeanStdev(shape=shape)
     for i in range(EMA_THRESHOLD):
         stats.update(data[i])
-        assert torch.allclose(stats.mean, torch.mean(data[0:i+1], dim=0))
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1], dim=0))
         if i > 0:
-            assert torch.allclose(stats.stdev, torch.std(data[0:i+1], dim=0, unbiased=False), atol=TOL)
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], dim=0, unbiased=False), atol=TOL
+            )
+
+    expected_mean = torch.mean(data[:EMA_THRESHOLD], dim=0)
+    expected_square_mean = torch.mean(data[:EMA_THRESHOLD] ** 2, dim=0)
+    for i in range(EMA_THRESHOLD, len(data)):
+        stats.update(data[i])
+        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1.0 - EMA_ALPHA)
+        expected_square_mean = expected_square_mean * EMA_ALPHA + data[i] ** 2 * (
+            1.0 - EMA_ALPHA
+        )
+        expected_stdev = torch.sqrt(expected_square_mean - expected_mean ** 2)
+        assert torch.allclose(stats.mean, expected_mean)
+        assert torch.allclose(stats.stdev, expected_stdev)
+
+
+def test_mean_condense_arithmetic():
+    """ Test computation of arithmetic mean in RunningMean when `condense=True`. """
+
+    # Set up case.
+    shape = (3, 3)
+    data = []
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data.append(torch.Tensor([[0, -1, -2], [-3, -4, -5], [-6, -7, -8]]))
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data = torch.stack(data)
+
+    # Perform and check computation.
+    mean = RunningMean(shape=shape, condense=True)
+    for i in range(len(data)):
+        mean.update(data[i])
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1]))
+
+
+def test_mean_condense_rand_arithmetic():
+    """
+    Test computation of arithmetic mean in RunningMean, with random values when
+    `condense=True`.
+    """
+
+    # Set up case.
+    shape = (10, 10)
+    data = torch.rand(EMA_THRESHOLD, *shape)
+
+    # Perform and check computation.
+    mean = RunningMean(shape=shape, condense=True)
+    for i in range(len(data)):
+        mean.update(data[i])
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1]))
+
+
+def test_mean_condense_ema():
+    """ Test computation of EMA in RunningMean when `condense=True`. """
+
+    # Set up case.
+    shape = (3, 3)
+    data = []
+    for i in range(EMA_THRESHOLD):
+        data.append(torch.ones(*shape))
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data.append(torch.Tensor([[0, -1, -2], [-3, -4, -5], [-6, -7, -8]]))
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data = torch.stack(data)
+
+    # Perform and check computation.
+    mean = RunningMean(shape=shape, condense=True)
+    for i in range(EMA_THRESHOLD):
+        mean.update(data[i])
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1]))
+
+    expected_mean = torch.ones(())
+    for i in range(EMA_THRESHOLD, len(data)):
+        mean.update(data[i])
+        expected_mean = expected_mean * EMA_ALPHA + torch.mean(data[i]) * (
+            1.0 - EMA_ALPHA
+        )
+        assert torch.allclose(mean.mean, expected_mean)
+
+
+def test_mean_condense_rand_ema():
+    """
+    Test computation of EMA in RunningMean, with random values when `condense=True`.
+    """
+
+    # Set up case.
+    shape = (10, 10)
+    data = torch.rand(EMA_THRESHOLD + 200, *shape)
+
+    # Perform and check computation.
+    mean = RunningMean(shape=shape, condense=True)
+    for i in range(EMA_THRESHOLD):
+        mean.update(data[i])
+        assert torch.allclose(mean.mean, torch.mean(data[0 : i + 1]))
+
+    expected_mean = mean.mean
+    for i in range(EMA_THRESHOLD, len(data)):
+        mean.update(data[i])
+        expected_mean = expected_mean * EMA_ALPHA + torch.mean(data[i]) * (
+            1.0 - EMA_ALPHA
+        )
+        assert torch.allclose(mean.mean, expected_mean)
+
+
+def test_mean_std_condense_arithmetic():
+    """
+    Test computation of arithmetic mean and standard deviation in RunningMean when
+    `condense=True`.
+    """
+
+    # Set up case.
+    shape = (3, 3)
+    data = []
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data.append(torch.Tensor([[0, -1, -2], [-3, -4, -5], [-6, -7, -8]]))
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data = torch.stack(data)
+
+    # Perform and check computation.
+    stats = RunningMeanStdev(shape=shape, condense=True)
+    for i in range(len(data)):
+        stats.update(data[i])
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1]))
+        if i > 0:
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], unbiased=False)
+            )
+
+
+def test_mean_std_condense_rand_arithmetic():
+    """
+    Test computation of arithmetic mean and standard deviation in RunningMean, with
+    random values when `condense=True`.
+    """
+
+    # Set up case.
+    shape = (10, 10)
+    data = torch.rand(EMA_THRESHOLD, *shape)
+
+    # Perform and check computation.
+    stats = RunningMeanStdev(shape=shape, condense=True)
+    for i in range(len(data)):
+        stats.update(data[i])
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1]))
+        if i > 0:
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], unbiased=False), atol=TOL
+            )
+
+
+def test_mean_std_condense_ema():
+    """
+    Test computation of EMA mean and standard deviation in RunningMean when
+    `condense=True`.
+    """
+
+    # Set up case.
+    shape = (3, 3)
+    data = []
+    for i in range(EMA_THRESHOLD):
+        data.append(torch.ones(*shape))
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data.append(torch.Tensor([[0, -1, -2], [-3, -4, -5], [-6, -7, -8]]))
+    data.append(torch.Tensor([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
+    data = torch.stack(data)
+
+    # Perform and check computation.
+    stats = RunningMeanStdev(shape=shape, condense=True)
+    for i in range(EMA_THRESHOLD):
+        stats.update(data[i])
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1]))
+        if i > 0:
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], unbiased=False), atol=TOL
+            )
+
+    expected_mean = torch.ones(())
+    expected_square_mean = torch.ones(())
+    for i in range(EMA_THRESHOLD, len(data)):
+        stats.update(data[i])
+        expected_mean = expected_mean * EMA_ALPHA + torch.mean(data[i]) * (
+            1.0 - EMA_ALPHA
+        )
+        expected_square_mean = expected_square_mean * EMA_ALPHA + torch.mean(
+            data[i] ** 2
+        ) * (1.0 - EMA_ALPHA)
+        expected_stdev = torch.sqrt(expected_square_mean - expected_mean ** 2)
+        assert torch.allclose(stats.mean, expected_mean)
+        assert torch.allclose(stats.stdev, expected_stdev)
+
+
+def test_mean_std_condense_rand_ema():
+    """
+    Test computation of EMA mean and standard deviation in RunningMean, with random
+    values when `condense=True`.
+    """
+
+    # Set up case.
+    shape = (10, 10)
+    data = torch.rand(EMA_THRESHOLD + 200, *shape)
+
+    # Perform and check computation.
+    stats = RunningMeanStdev(shape=shape, condense=True)
+    for i in range(EMA_THRESHOLD):
+        stats.update(data[i])
+        assert torch.allclose(stats.mean, torch.mean(data[0 : i + 1]))
+        if i > 0:
+            assert torch.allclose(
+                stats.stdev, torch.std(data[0 : i + 1], unbiased=False), atol=TOL
+            )
 
     expected_mean = torch.mean(data[:EMA_THRESHOLD])
     expected_square_mean = torch.mean(data[:EMA_THRESHOLD] ** 2)
     for i in range(EMA_THRESHOLD, len(data)):
         stats.update(data[i])
-        expected_mean = expected_mean * EMA_ALPHA + data[i] * (1. - EMA_ALPHA)
-        expected_square_mean = expected_square_mean * EMA_ALPHA + data[i] ** 2 * (1. - EMA_ALPHA)
+        expected_mean = expected_mean * EMA_ALPHA + torch.mean(data[i]) * (
+            1.0 - EMA_ALPHA
+        )
+        expected_square_mean = expected_square_mean * EMA_ALPHA + torch.mean(
+            data[i] ** 2
+        ) * (1.0 - EMA_ALPHA)
         expected_stdev = torch.sqrt(expected_square_mean - expected_mean ** 2)
         assert torch.allclose(stats.mean, expected_mean)
         assert torch.allclose(stats.stdev, expected_stdev)
-
