58fd7280142fdc518ea9d6f384d14497969f5fb9
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2021-07-17 01:11:50 -0400

    Finished first version of CKD script, which now can approximate the output of a single network with a polynomial.

--- scripts/ckd_sandbox.py
+++ scripts/ckd_sandbox.py
@@ -10,15 +10,16 @@ from typing import List
 
 import torch
 import torch.nn as nn
-from sympy import Symbol, Expr, expand
+import sympy
+from sympy import expand, Symbol, Expr, Number, Add, Mul, Pow
 
 
-INPUT_SIZE = 4
+INPUT_SIZE = 2
 OUTPUT_SIZE = 2
 NUM_LAYERS = 3
-HIDDEN_SIZE = 3
-MAX_PS_DEGREE = 3
-BATCH_SIZE = 4
+HIDDEN_SIZE = 2
+MAX_PS_DEGREE = 4
+BATCH_SIZE = 5
 
 
 class ExpActivation(nn.Module):
@@ -62,9 +63,7 @@ def network_to_expr(
         outputs = []
         for j in range(linear.out_features):
             outputs.append(
-                sum(
-                    inputs[k] * weights[i][j][k] for k in range(linear.in_features)
-                )
+                sum(inputs[k] * weights[i][j][k] for k in range(linear.in_features))
                 + biases[i][j]
             )
 
@@ -78,22 +77,160 @@ def network_to_expr(
             # representation of the exponential function.
             for i in range(len(outputs)):
                 outputs[i] = sum(
-                    outputs[i] ** m / factorial(m)
-                    for m in range(MAX_PS_DEGREE)
+                    outputs[i] ** m / factorial(m) for m in range(MAX_PS_DEGREE)
                 )
-                outputs[i] = expand(outputs[i])
 
         inputs = outputs
+
+    # Expand outputs.
+    for i in range(len(outputs)):
+        outputs[i] = expand(outputs[i])
 
     return outputs
 
 
-def estimate_output(input_data: torch.Tensor, network_expr: Expr) -> torch.Tensor:
+def is_constant(expr: Expr) -> bool:
+    """
+    Whether or not `expr` is a constant, i.e. a Number or Symbol.
+    """
+    return any(issubclass(expr.func, cls) for cls in [Number, Symbol])
+
+
+def is_monomial(expr: Expr) -> bool:
+    """
+    Whether or not `expr` is a monomial, i.e. a product of constants and symbols.
+    """
+
+    monomial = True
+
+    if is_constant(expr):
+        pass
+
+    elif expr.func == Pow:
+        monomial = is_monomial(expr.args[0]) and is_constant(expr.args[1])
+
+    elif expr.func == Mul:
+        for subexpr in expr.args:
+            constant = is_constant(subexpr)
+            constant_power = (
+                subexpr.func == Pow
+                and is_constant(subexpr.args[0])
+                and is_constant(subexpr.args[1])
+            )
+            monomial = monomial and (constant or constant_power)
+            if not monomial:
+                break
+
+    else:
+        monomial = False
+
+    return monomial
+
+
+def evaluate_symbol(
+    input_data: torch.Tensor, network: nn.Module, symbol: Symbol
+) -> torch.Tensor:
+    """
+    Evaluate a symbol representing an input element or a network parameter, and return
+    the corresponding torch Tensor.
+    """
+
+    if symbol.name.startswith("x"):
+        idx = int(symbol.name[1:])
+        val = input_data[idx]
+
+    elif symbol.name.startswith("w"):
+        first_pos = symbol.name.find("_")
+        second_pos = symbol.name.find("_", first_pos + 1)
+        layer = int(symbol.name[1:first_pos])
+        out_unit = int(symbol.name[first_pos + 1 : second_pos])
+        in_unit = int(symbol.name[second_pos + 1 :])
+        val = network[layer][0].weight[out_unit, in_unit]
+
+    elif symbol.name.startswith("b"):
+        pos = symbol.name.find("_")
+        layer = int(symbol.name[1:pos])
+        out_unit = int(symbol.name[pos + 1 :])
+        val = network[layer][0].bias[out_unit]
+
+    else:
+        raise ValueError(
+            "Can only handle symbols with names starting with 'x', 'w', or 'b'."
+        )
+
+    return val
+
+
+def evaluate_monomial(
+    input_data: torch.Tensor, network: nn.Module, monomial: Expr
+) -> torch.Tensor:
+    """
+    Numberically evaluate a monomial expression by substituting torch tensors in for the
+    symbols, returning a torch Tensor.
+    """
+
+    if issubclass(monomial.func, Number):
+        val = torch.Tensor([float(monomial)])
+
+    elif monomial.func == Symbol:
+        val = evaluate_symbol(input_data, network, monomial)
+
+    elif monomial.func == Pow:
+        base, exp = monomial.args
+        if issubclass(base.func, Number):
+            base_val = torch.Tensor([float(base)])
+        else:
+            base_val = evaluate_symbol(input_data, network, base)
+        exp_val = torch.Tensor([float(exp)])
+        val = torch.pow(base_val, exp_val)
+
+    elif monomial.func == Mul:
+        val = torch.Tensor([1])
+        for subexpr in monomial.args:
+            if issubclass(subexpr.func, Number):
+                val *= torch.Tensor([float(subexpr)])
+            elif subexpr.func == Symbol:
+                val *= evaluate_symbol(input_data, network, subexpr)
+            elif subexpr.func == Pow:
+                base, exp = subexpr.args
+                if issubclass(base.func, Number):
+                    base_val = torch.Tensor([float(base)])
+                else:
+                    base_val = evaluate_symbol(input_data, network, base)
+                exp_val = torch.Tensor([float(exp)])
+                val *= torch.pow(base_val, exp_val)
+            else:
+                assert False
+    else:
+        assert False
+
+    return val
+
+
+def estimate_output(
+    input_data: torch.Tensor, network: nn.Module, network_expr: List[Expr]
+) -> torch.Tensor:
     """
     Estimate the output of the neural network whose approximate expression is
-    `network_expr` by evaluating the expression on input `input_data.
+    `network_expr` by evaluating the expression on input `input_data`. Input data should
+    have shape `(INPUT_SIZE,)`.
     """
-    raise NotImplementedError
+
+    # Check that `network_expr` is a sum of monomials.
+    for expr in network_expr:
+        assert expr.func == Add
+        for subexpr in expr.args:
+            assert is_monomial(subexpr)
+
+    # Construct output from network expression.
+    estimated_output = torch.zeros(len(network_expr))
+    for i, expr in enumerate(network_expr):
+        for subexpr in expr.args:
+            estimated_output[i] += evaluate_monomial(
+                input_data, network, subexpr
+            ).squeeze()
+
+    return estimated_output
 
 
 def main():
@@ -139,7 +276,9 @@ def main():
 
     # Get actual network output, estimated output, and compare.
     actual_output = network(input_data)
-    estimated_output = estimate_output(input_data, network_expr)
+    estimated_output = torch.zeros_like(actual_output)
+    for i in range(input_data.shape[0]):
+        estimated_output[i] = estimate_output(input_data[i], network, network_expr)
     error = torch.mean((actual_output - estimated_output) ** 2).item()
     print(f"Mean error: {error}")
 
